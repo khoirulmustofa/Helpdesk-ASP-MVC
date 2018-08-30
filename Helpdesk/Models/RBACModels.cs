7cca47da3797220355467def4efa96cc12448304
khoirulmustofa <khoirul.mustofa2014@gmail.com>
2018-08-30 12:50:08 +0700

    Upload Basic RBAC

---
+++ Helpdesk/Models/RBACModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Helpdesk.Models
+{
+    [Table("PERMISSIONS")]
+    public class PERMISSION
+    {
+        [Key]
+        public int PermissionId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string PermissionDescription { get; set; }
+
+        public virtual List<ApplicationRole> ROLES { get; set; }
+    }   
+}
---
+++ Helpdesk/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Helpdesk.Startup))]
+namespace Helpdesk
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
